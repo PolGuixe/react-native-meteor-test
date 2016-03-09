124c09ca23d63aee58c6f60709ff26822b143892
PolGuixe <pol.guixe@gmail.com>
2016-03-10 00:28:35 +0100

    basic setup

--- tests.js
+++
@@ -1,8 +0,0 @@
-import assert from "assert";
-import {name as appName} from "./react-native-meteor-test.js";
-
-describe("react-native-meteor-test", () => {
-  it("should export its name", () => {
-    assert.strictEqual(appName, "react-native-meteor-test");
-  });
-});
